fb58d81b96dd3bacd741a668dec72305bed6b58d
Reuel D'silva <reuelrds@gmail.com>
2019-05-05 15:53:55 +0530

    Added method to fetch data from unsplash api and some minor changes

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -3,6 +3,7 @@ import argparse
 import json
 import uniqid
 import sys
+import requests
 
 def connect_database(database_path):
   """
@@ -43,6 +44,7 @@ def create_tables(connection):
         thumb     Text,
         photo_id  Text,
         FOREIGN KEY (photo_id) REFERENCES Photo(photo_id)
+          ON UPDATE CASCADE
           ON DELETE CASCADE
       )
       """
@@ -59,6 +61,7 @@ def create_tables(connection):
         profile_image Text,
         photo_id      Text,
         FOREIGN KEY (photo_id) REFERENCES Photo(photo_id)
+          ON UPDATE CASCADE
           ON DELETE CASCADE
 
       )
@@ -69,15 +72,27 @@ def create_tables(connection):
     print(error)
 
 
-def fetch_data():
+def fetch_data(access_key, query, page, page_size):
   """
     Fetches data from Unsplash API
   """
 
-  with open("./test_data.json", "r") as f:
-    dictdump = json.load(f)
+  if not access_key:
+    with open("./test_data.json", "r") as f:
+      data_dict = json.load(f)
+  else:
+    api_url = "http://api.unsplash.com/search/photos"
+    fetched_data = requests.get(
+      "{api_url}?client_id={access_key}&query={query}&page={page}&per_page={page_size}".format(
+        api_url = api_url,
+        access_key = access_key,
+        query = query,
+        page = page,
+        page_size = page_size
+      ))
 
-  return dictdump
+    data_dict = fetched_data.json()
+  return data_dict
 
 
 def parse_data(data_items):
@@ -104,7 +119,7 @@ def parse_data(data_items):
     url["thumb"] = data_item["urls"]["thumb"]
     url["photo_id"] = data_item["id"]
 
-    user["user_id"] = data_item["user"]["id"]
+    user["user_id"] = uniqid.uniqid()
     user["username"] = data_item["user"]["username"]
     user["name"] = data_item["user"]["name"]
     user["link"] = data_item["user"]["links"]["html"]
@@ -121,74 +136,78 @@ def load_data(connection, data_items):
     Loads Image data into the database
   """
   for item in data_items:
-    photo_data, url_data, user_data = item
-    connection.execute(
-      """
-        INSERT INTO Photo VALUES (
-          "{photo_id}",
-          "{description}",
-          "{alt_description}",
-          "{likes}",
-          "{tags}"
+    try:
+      photo_data, url_data, user_data = item
+      connection.execute(
+        """
+          INSERT INTO Photo VALUES (
+            "{photo_id}",
+            "{description}",
+            "{alt_description}",
+            "{likes}",
+            "{tags}"
+          )
+        """.format(
+          photo_id = photo_data["photo_id"],
+          description = photo_data["description"],
+          alt_description = photo_data["alt_description"],
+          likes = photo_data["likes"],
+          tags = photo_data["tags"]
         )
-      """.format(
-        photo_id = photo_data["photo_id"],
-        description = photo_data["description"],
-        alt_description = photo_data["alt_description"],
-        likes = photo_data["likes"],
-        tags = photo_data["tags"]
       )
-    )
 
-    connection.execute(
-      """
-        INSERT INTO Url VALUES (
-          "{url_id}",
-          "{raw}",
-          "{full}",
-          "{regular}",
-          "{small}",
-          "{thumb}",
-          "{photo_id}"
+      connection.execute(
+        """
+          INSERT INTO Url VALUES (
+            "{url_id}",
+            "{raw}",
+            "{full}",
+            "{regular}",
+            "{small}",
+            "{thumb}",
+            "{photo_id}"
+          )
+        """.format(
+          url_id = url_data["url_id"],
+          raw = url_data["raw"],
+          full = url_data["full"],
+          regular = url_data["regular"],
+          small = url_data["small"],
+          thumb = url_data["thumb"],
+          photo_id = url_data["photo_id"],
         )
-      """.format(
-        url_id = url_data["url_id"],
-        raw = url_data["raw"],
-        full = url_data["full"],
-        regular = url_data["regular"],
-        small = url_data["small"],
-        thumb = url_data["thumb"],
-        photo_id = url_data["photo_id"],
       )
-    )
 
-    connection.execute(
-      """
-        INSERT INTO User VALUES (
-          "{user_id}",
-          "{username}",
-          "{name}",
-          "{link}",
-          "{profile_image}",
-          "{photo_id}"
+      connection.execute(
+        """
+          INSERT INTO User VALUES (
+            "{user_id}",
+            "{username}",
+            "{name}",
+            "{link}",
+            "{profile_image}",
+            "{photo_id}"
+          )
+        """.format(
+          user_id = user_data["user_id"],
+          username = user_data["username"],
+          name = user_data["name"],
+          link = user_data["link"],
+          profile_image = user_data["profile_image"],
+          photo_id = user_data["photo_id"],
         )
-      """.format(
-        user_id = user_data["user_id"],
-        username = user_data["username"],
-        name = user_data["name"],
-        link = user_data["link"],
-        profile_image = user_data["profile_image"],
-        photo_id = user_data["photo_id"],
       )
-    )
+    except sqlite3.Error as error:
+      print(error)
+      continue
 
 
-def main(db_path):
+def main(access_key, db_path, query, page, page_size):
   try:
     connection = connect_database(db_path)
 
     create_tables(connection)
-    data = fetch_data()
+    data = fetch_data(access_key, query, page, page_size)
     data = parse_data(data['results'])
     load_data(connection, data)
 
@@ -202,8 +221,32 @@ def main(db_path):
 if __name__=="__main__":
 
   # Get DB path from user
-  parser = argparse.ArgumentParser(description="Fetches and Loads Photos into the Database")
-  parser.add_argument("--db_path", default="../backend/database/photos.db", help="Path to Photos Database")
+  parser = argparse.ArgumentParser(
+    description="Fetches and Loads Photos from Unsplash into the Database"
+  )
+
+  parser.add_argument(
+    "access_key",
+    help="Unsplash API access key"
+  )
+
+  parser.add_argument(
+    "--db_path",
+    default="../backend/database/photos.db",
+    help="Path to Photos Database")
+  parser.add_argument(
+    "--query",
+    default="landscape,nature",
+    help="Comma separatd query terms for searching photos")
+  parser.add_argument(
+    "--page",
+    default="1",
+    help="Result page number")
+  parser.add_argument(
+    "--page_size",
+    default="30",
+    help="Number of results per page. Max 30")
+
   parser = parser.parse_args()
 
-  main(parser.db_path)
+  main(parser.access_key, parser.db_path, parser.query, parser.page, parser.page_size)
