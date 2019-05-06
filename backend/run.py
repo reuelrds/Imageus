ca6ccca59f6ecdc53abf63bee04ededf6c4978ce
Reuel D'silva <reuelrds@gmail.com>
2019-05-06 13:42:22 +0530

    _

--- backend/run.py
+++ backend/run.py
@@ -4,8 +4,14 @@
   A Flask REST Api Backend for Imageus Website
 """
 from app import create_app
+from app.extensions import DB
+from app.models import User, Photo, Url
 
 APP = create_app()
+
+@APP.shell_context_processor
+def make_shell_context():
+  return {'db': DB, 'User': User, 'Photo': Photo, 'Url': Url}
 
 if __name__ == "__main__":
   APP.run()
