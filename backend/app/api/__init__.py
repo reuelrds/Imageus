582b00f335ea3724af6397c8edb7ae523193c0cb
Reuel D'silva <reuelrds@gmail.com>
2019-05-07 04:19:00 +0530

    Fixed Cors error

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -3,7 +3,9 @@ Main API Package
 This package defines routes
 """
 from flask import Blueprint
+from flask_cors import CORS
 
 BLUEPRINT = Blueprint('api', __name__)
+CORS(BLUEPRINT)
 
 from . import photos
