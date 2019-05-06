6cc82f25c3ff235e44c16f54cd0ace6cf5b14093
Reuel D'silva <reuelrds@gmail.com>
2019-05-06 13:51:24 +0530

    Defined photos route

--- backend/app/api/photos.py
+++ backend/app/api/photos.py
@@ -3,11 +3,44 @@
 """
 
 from flask import jsonify
+
+from app.models import Photo, Url, User
 from . import BLUEPRINT as bp
 
-@bp.route('/test', methods=['GET'])
+@bp.route('/photos', methods=['GET'])
 def test():
   """
-    Test Route
+    Returns an array of all photos stored in the database as a json object
   """
-  return jsonify("Hey! It's Working!!")
+  # Join all Tables
+  data_items = Photo.query \
+    .add_columns(
+      Photo.photo_id,
+      Photo.description,
+      Photo.alt_description,
+      Photo.likes,
+      Photo.tags,
+
+      Url.raw,
+      Url.full,
+      Url.regular,
+      Url.small,
+      Url.thumb,
+
+      User.username,
+      User.name,
+      User.link,
+      User.profile_image
+    ) \
+    .join(Url) \
+    .join(User) \
+    .all()
+
+  # Create a new Object
+  result = []
+  for item in data_items:
+    photo = item._asdict()
+    del photo['Photo']
+    result.append(photo)
+
+  return jsonify(result)
