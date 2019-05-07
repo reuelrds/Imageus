7419d81c4ea545dae2c4da1af9c48018716c69d7
Reuel D'silva <reuelrds@gmail.com>
2019-05-07 05:54:22 +0530

    added masonry layout

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,58 +1,58 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnChanges, ViewChildren, QueryList } from '@angular/core';
 
 import * as Masonry from 'masonry-layout';
 import { MatDialog } from '@angular/material';
 import { ImageDetailsComponent } from '../image-details/image-details.component';
+import { ImageService } from '../services/image.service';
+import { Image } from '../models/image';
 
 @Component({
   selector: 'imageus-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.scss']
 })
-export class ImageComponent implements OnInit, AfterViewInit {
+export class ImageComponent implements OnInit, AfterViewInit, OnChanges {
 
 
 
   @ViewChild('imageContainer') imageContainer: ElementRef;
+  @ViewChildren('ele') ele: QueryList<'ele'>;
 
-  data = {
-    alt_description: 'Boxed Water cartons on the black and white ground',
-    description: 'None',
-    full: 'https://images.unsplash.com/photo-1553531087-b25a0b9a68ab?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjY5ODM3fQ',
-    likes: 40,
-    link: 'https://unsplash.com/@boxedwater',
-    name: 'Boxed Water Is Better',
-    photo_id: 'M6eWvLb2EYY',
-    profile_image: 'https://images.unsplash.com/profile-1553620527727-3d787a876895?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64',
-    raw: 'https://images.unsplash.com/photo-1553531087-b25a0b9a68ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjY5ODM3fQ',
-    regular: 'https://images.unsplash.com/photo-1553531087-b25a0b9a68ab?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjY5ODM3fQ',
-    small: 'https://images.unsplash.com/photo-1553531087-b25a0b9a68ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjY5ODM3fQ',
-    tags: '',
-    thumb: 'https://images.unsplash.com/photo-1553531087-b25a0b9a68ab?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjY5ODM3fQ',
-    username: 'boxedwater'
-  };
+  images: Image[];
+  mas;
 
-
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private imageService: ImageService) { }
 
   ngOnInit() {
+    this.imageService.get_photos().subscribe(res => {
+      this.images = res;
+    });
   }
 
   ngAfterViewInit() {
-    const mas = new Masonry(this.imageContainer.nativeElement, {
+    this.mas = new Masonry(this.imageContainer.nativeElement, {
       itemSelector: '.box',
       columnWidth: '.box-sizer',
       percentPosition: true,
       gutter: '.box-gutter'
     });
 
+    this.ele.changes.subscribe(el => {
+      el.toArray().forEach(res => {
+        this.mas.appended(res.nativeElement);
+      });
+    });
   }
 
-  showImage() {
+  ngOnChanges() {
+
+  }
+
+  showImage(event) {
     this.dialog.open(ImageDetailsComponent, {
       width: '80vw',
       height: '90vh',
-      data: this.data,
+      data: event,
       autoFocus: false
     });
   }
