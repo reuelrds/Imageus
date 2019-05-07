f3732afbe6f779b43e1021e2f47a43bea83172d0
Reuel D'silva <reuelrds@gmail.com>
2019-05-08 04:10:42 +0530

    setup template for favourite dialog

--- src/app/favourites-dialog/favourites-dialog.component.ts
+++ src/app/favourites-dialog/favourites-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { Image } from '../models/image';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'imageus-favourites-dialog',
@@ -9,12 +10,19 @@ import { Image } from '../models/image';
 })
 export class FavouritesDialogComponent implements OnInit {
 
+  email: FormControl;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: Image[]
   ) { }
 
   ngOnInit() {
+    this.email = new FormControl('', Validators.email);
     console.log(this.data);
+  }
+
+  sendEmail() {
+    console.log(this.email.value);
   }
 
 }
