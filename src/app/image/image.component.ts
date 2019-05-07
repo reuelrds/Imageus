3f1e17e4c30266e7611760997e46dade66486378
Reuel D'silva <reuelrds@gmail.com>
2019-05-08 00:29:41 +0530

    replaced masonry library with packery for image layouts

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,59 +1,66 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ViewChildren, QueryList, Input, OnChanges } from '@angular/core';
 
-import * as Masonry from 'masonry-layout';
+import * as Packery from 'packery';
 import * as imagesLoaded from 'imagesloaded';
 
 import { MatDialog } from '@angular/material';
 import { ImageDetailsComponent } from '../image-details/image-details.component';
 import { ImageService } from '../services/image.service';
 import { Image } from '../models/image';
-import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'imageus-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.scss']
 })
-export class ImageComponent implements OnInit, AfterViewInit {
+export class ImageComponent implements OnInit, AfterViewInit, OnChanges {
 
 
 
   @ViewChild('imageContainer') imageContainer: ElementRef;
   @ViewChildren('ele') ele: QueryList<ElementRef>;
 
-  images: Image[];
+  @Input() images: Image[] = [];
+
+  // images: Image[];
   mas;
   imLoad;
+  prevLoadedItems = [];
+
+  masOpt =  {
+    itemSelector: '.box',
+    columnWidth: '.box',
+    percentPosition: true,
+    gutter: '.box-gutter',
+    initLayout: true,
+    horizontalOrder: false,
+    originTop: true,
+    originLeft: true
+  };
 
   constructor(private dialog: MatDialog, private imageService: ImageService) { }
 
   ngOnInit() {
-    this.imageService.get_photos().subscribe(res => {
-      this.images = res;
-    });
-    this.mas = new Masonry(this.imageContainer.nativeElement, {
-      itemSelector: '.box',
-      columnWidth: '.box-sizer',
-      percentPosition: true,
-      gutter: '.box-gutter',
-      initLayout: false,
-      horizontalOrder: false
-    });
+
+  }
+
+  ngOnChanges() {
+    if (this.images){
+      console.log(this.images.length);
+      // this.mas.layout();
+      // this.mas.reloadItems();
+    }
   }
 
   ngAfterViewInit() {
-    this.ele.changes.subscribe(result => {
-      this.ele.toArray().forEach((el, index, array) => {
-        this.mas.addItems(el.nativeElement);
-        imagesLoaded(el.nativeElement).on('progress', (instance, image) => {
-          if (image.img.src.includes('photo')) {
-            this.mas.layout();
-            el.nativeElement.style.opacity = 1;
-          }
+    this.ele.changes.subscribe(res => {
+      const pckry = new Packery(this.imageContainer.nativeElement, this.masOpt);
+      this.ele.toArray().forEach(el => {
+        imagesLoaded(el.nativeElement).on('progress', () => {
+          pckry.layout();
+          el.nativeElement.style.opacity = 1;
         });
       });
-
-      this.ele.toArray().forEach(el => el.nativeElement.style.opacity = 1);
     });
   }
 
