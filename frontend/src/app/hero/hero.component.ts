bdd0afb6b85878c79025d3d089b5cb3ca842f6bf
reuelrds <reuelrds@gmail.com>
2020-04-28 02:36:40 +0530

    Added Hero Component

---
+++ frontend/src/app/hero/hero.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'imageus-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+export class HeroComponent implements OnInit {
+
+  searchBox: FormControl;
+
+  @Output() heroSearchQuery = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.searchBox = new FormControl('');
+  }
+
+  searchPhotos() {
+    this.heroSearchQuery.emit(this.searchBox.value);
+
+  }
+}
