99834db976b54de8a64591a95ff1921714558c5e
reuelrds <reuelrds@gmail.com>
2020-04-28 02:36:26 +0530

    Added NavBar

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import { Image } from './../shared/models/image';
+
+@Component({
+  selector: 'imageus-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit, OnChanges {
+
+  @Input() favImagesCount: number;
+  @Input() favImages: Image[];
+
+  isFavImagesCountZero = true;
+
+  constructor(
+    private dialog: MatDialog
+  ) { }
+
+  ngOnInit() {
+    console.log(this.favImages);
+  }
+
+  ngOnChanges() {
+    this.isFavImagesCountZero = this.favImagesCount > 0 ? false : true;
+  }
+
+  openFavouritesDialog() {
+    // this.dialog.open(FavouritesDialogComponent, {
+      // width: '80vw',
+      // height: '80vh',
+      // data: this.favImages,
+      // autoFocus: false
+    // });
+  }
+}
