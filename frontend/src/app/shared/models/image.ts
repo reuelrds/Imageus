c684dcee0623017451b9bc049b56664de6f9fcfe
reuelrds <reuelrds@gmail.com>
2020-04-28 01:52:51 +0530

    Added an Image interface

---
+++ frontend/src/app/shared/models/image.ts
@@ -0,0 +1,17 @@
+export interface Image {
+  alt_description: string;
+  description: string;
+  full: string;
+  likes: string;
+  link: string;
+  name: string;
+  photo_id: string;
+  profile_image: string;
+  raw: string;
+  regular: string;
+  small: string;
+  tags: string;
+  thumb: string;
+  username: string;
+  liked?: boolean;
+}
