3637dc1cfb44d6236034e33260f34a29b7371862
reuelrds <reuelrds@gmail.com>
2020-04-28 02:37:13 +0530

    Updated APp Module

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Image } from './shared/models/image';
+
 @Component({
   selector: 'imageus-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  favImagesCount = 0;
+  images: Image[];
+  likedImages: Image[] = [];
+  filteredImages: Image[];
+
+  searchQuery($event) {}
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,32 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MaterialModule } from './shared/material/material.module';
+import { SearchComponent } from './search/search.component';
+import { ThemePickerModule } from './theme-picker/theme-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    SearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    ThemePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
