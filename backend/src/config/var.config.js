021d82c4d749f1c42aa3d56f600334a94a4bd3e4
reuelrds <reuelrds@gmail.com>
2020-04-29 12:45:57 +0530

    Updated to connect to mongodb with authentication

--- backend/src/config/var.config.js
+++ backend/src/config/var.config.js
@@ -6,11 +6,11 @@ dotenv.config();
 const config = {
   PORT: process.env.PORT || "3000",
   mongodb: {
-    url: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`,
+    url: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}?authSource=admin`,
   },
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   SENDGRID_EMAIL_ID: process.env.SENDGRID_EMAIL_ID,
-  SENDGRID_TEMPLATE_ID: process.env.SENDGRID_TEMPLATE_ID
+  SENDGRID_TEMPLATE_ID: process.env.SENDGRID_TEMPLATE_ID,
 };
 
 module.exports = config;
